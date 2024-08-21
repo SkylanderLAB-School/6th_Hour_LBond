a4e26eceae4cf31d0c6337a00733e81b8fd69ff4
Logan Bond <lb125093@wpsok.org>
2024-08-21 14:07:24 -0500

    Added HW2 (not done)

---
+++ HW2.py
@@ -0,0 +1,18 @@
+#Name: Logan Bond
+#Class: 6th Hour
+#Assignment: HW2
+
+print("Hello World!")
+
+name = input("What is your name?:")
+print("Your name is:",name,"")
+
+x = input("Gimmie a number:")
+y = input("Now do it again:")
+print(x, "Good job")
+print(y, "Ok, now go to task 3")
+
+x=int
+y=int
+
+print(x + y)
