08172b057bcce0c6077ef85660490416b77747b4
Logan Bond <lb125093@wpsok.org>
2024-08-22 13:42:33 -0500

    Finished the assignment

--- HW2.py
+++ HW2.py
@@ -2,17 +2,18 @@
 #Class: 6th Hour
 #Assignment: HW2
 
+#print a hello world statement
 print("Hello World!")
 
+#make an input to ask for your name
 name = input("What is your name?:")
 print("Your name is:",name,"")
 
-x = input("Gimmie a number:")
-y = input("Now do it again:")
-print(x, "Good job")
-print(y, "Ok, now go to task 3")
+#make an input asking for 2 numbers
+num1 = int(input("Gimmie a number:"))
+num2 = int(input("Now do it again:"))
+print(num1, "Good job")
+print(num2, "Ok, now go to task 3")
 
-x=int
-y=int
-
-print(x + y)
+#add those two numbers together
+print("The sum of the two numbers is:", num1 + num2)
