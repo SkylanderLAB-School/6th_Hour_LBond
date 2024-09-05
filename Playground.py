513a22cba03d891bc75421212d02db1b36f9aa53
Logan Bond <lb125093@wpsok.org>
2024-09-05 14:04:38 -0500

    learned what IF does

--- Playground.py
+++ Playground.py
@@ -1,10 +1,47 @@
 # Name: Logan Bond
 # Class: 6th Hour
 # Assignment: Playground
-# Code used:
+# Help From : Kyle
 
 print("Hello World")
 
-numlist = [1,2,3,4,5,6,8492637]
+#while True:
+#    location = input("Where do you want to go?:")
 
-input("Please put a number:")
+#    if location == "Monkey Bars" :
+#        print("Sorry, they were removed for some stupid kids breaking 3 ribs on them.")
+
+#    if location == "Metal Slide" :
+#        print("Sorry, they were removed for kids losing their entire back from 427th degree burns.")
+
+#    if location == "Swing Set" :
+#        print("Well, there is a swing open, but it's the 'bad swing'. Maybe something else would be more fun.")
+
+#    if location == "Home" :
+#        print("you cant go home.")
+
+#    if location == "Merry-Go-Round":
+#        print("Good choice!")
+#        break
+
+print("Now comes an important question. Go left or go right?")
+direction = input("Go ")
+
+if direction == "left" :
+    print("Alright, then start spinning left!")
+
+
+if direction == "right" :
+    print("As you start to spin to the right, a squeaky wheel begins to squeak")
+    print("This causes your ears to bleed and you to go to the hospital.")
+    print("[Insert Dark Souls: 'YOU DIED' here]")
+
+print("As you spin left, you go faster and faster, even without another master!")
+print("But, just as you started to have fun, your mom says it's time to go home...")
+print("Goodbye")
+
+ending = input("GAME OVER, RESTART?")
+if ending == "yes":
+    print("Too bad, womp womp, you gotta do chores.")
+
+if ending ==
