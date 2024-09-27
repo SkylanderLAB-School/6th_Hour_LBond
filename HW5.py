a6751a72c035e87ecb179b79e36c565d1d7ace29
Logan Bond <lb125093@wpsok.org>
2024-09-27 13:44:29 -0500

    Finished HW5.

---
+++ HW5.py
@@ -0,0 +1,18 @@
+#Name: Logan Bond
+#Class: 6th Hour
+#Assignment: HW5
+
+#1. Print "Hello World!"
+print("Hello World!")
+
+#2. Create a list that contains 3 integers
+numlist = [7,427,6127]
+
+#3. Create an if statement that prints out whichever of the three numbers is the highest
+if numlist[0] >= numlist[1] and numlist[2] :
+    print(numlist[0], "is the biggest number.")
+elif numlist[1] >= numlist[2] and numlist[0] :
+    print(numlist[1], "is the biggest number.")
+elif numlist[2] >= numlist[1] and numlist[0] :
+    print(numlist[2], "is the biggest number.")
+
