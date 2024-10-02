5d009180bd0309cfc252fa3d6eded4959cb346ce
Logan Bond <lb125093@wpsok.org>
2024-10-02 14:07:15 -0500

    Always do things the lazy way.

--- HW5.py
+++ HW5.py
@@ -2,18 +2,21 @@
 #Class: 6th Hour
 #Assignment: HW5
 
+import random
 #1. Print "Hello World!"
 print("Hello World!")
 
 #2. Create a list that contains 3 integers
-numlist = [7,427,6127]
+numlist = [random.randint(1,1000), random.randint(1,1000), random.randint(1,1000)]
 print(numlist)
 
 #3. Create an if statement that prints out whichever of the three numbers is the highest
-if numlist[0] >= numlist[1] and numlist[2] :
+if numlist[0] >= numlist[1] and numlist[0] >= numlist[2] :
     print(numlist[0], "is the biggest number.")
-elif numlist[1] >= numlist[2] and numlist[0] :
+elif numlist[1] >= numlist[2] and numlist[1] >= numlist[0]  :
     print(numlist[1], "is the biggest number.")
-elif numlist[2] >= numlist[1] and numlist[0] :
+elif numlist[2] >= numlist[1] and numlist[2] >= numlist[0] :
     print(numlist[2], "is the biggest number.")
+
+print(max(numlist), "is the biggest number")
 
