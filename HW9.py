d7fd45e2908dd5bf0b7ebf859674e97c08629867
Logan Bond <lb125093@wpsok.org>
2024-10-24 13:40:13 -0500

    Finished HW9

---
+++ HW9.py
@@ -0,0 +1,30 @@
+#Name: Logan Bond
+#Class: 6th Hour
+#Assignment: HW9
+
+import random
+
+#1. Print Hello World!
+print("Hello World!")
+
+#2. Go to the link below and convert the code into pseudocode in comments, then code the flowchart completely:
+#https://adacomputerscience.org/images/content/computer_science/design_and_development/program_design/figures/ada_cs_design_flow_ifelseif.svg
+
+#unga bunga
+#make temp a var with a num
+#if temp > 20 = HOTOTOTOTOTOTOT
+#if tem no = go to if temp > 10 = it mild salsa
+#in neither goooood = it nice weather(cold)
+#after all, print tank you
+
+temp = int(random.randint(0,27))
+
+if temp > 20:
+    print("It's ''hot'' outside")
+    print("Thank you.")
+elif temp > 10:
+    print("It's mild outside")
+    print("Thank you.")
+else:
+    print("It's perfect weather(cold) outside")
+    print("Thank you.")
