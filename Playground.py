e89a88c79136c4408944b86834795c59cfacad05
Logan Bond <lb125093@wpsok.org>
2024-11-01 13:31:04 -0500

    fixed some crap in the PG

--- Playground.py
+++ Playground.py
@@ -43,7 +43,7 @@ if direction == "right" :
     time.sleep(1.5)
     print("This causes your ears to bleed and you to go to the hospital.")
     time.sleep(1.5)
-    print("[Insert Dark Souls: 'YOU DIED' here]")
+    print("[Insert Dark Souls: YOU DIED here]")
 time.sleep(1.5)
 ending = input("GAME OVER, RESTART?:")
 if ending == "Yes":
@@ -90,7 +90,7 @@ if ending == "^^vv<><>BA-" :
         time.sleep(1.5)
         print("This causes your ears to bleed and you to go to the hospital.")
         time.sleep(1.5)
-        print("[Insert Dark Souls: 'YOU DIED' here]")
+        print("[Insert Dark Souls: YOU DIED here]")
     time.sleep(1.5)
     ending = input("GAME OVER, RESTART?:")
     if ending == "Yes":
@@ -146,7 +146,7 @@ if ending == "^^vv<><>BA-" :
             time.sleep(1.5)
             print("This causes your ears to bleed and you to go to the hospital.")
             time.sleep(1.5)
-            print("[Insert Dark Souls: 'YOU DIED' here]")
+            print("[Insert Dark Souls: YOU DIED here]")
         time.sleep(1.5)
         ending = input("GAME OVER, RESTART?:")
         if ending == "Yes":
@@ -201,7 +201,7 @@ if ending == "^^vv<><>BA-" :
                 time.sleep(1.5)
                 print("This causes your ears to bleed and you to go to the hospital.")
                 time.sleep(1.5)
-                print("[Insert Dark Souls: 'YOU DIED' here]")
+                print("[Insert Dark Souls: YOU DIED here]")
             time.sleep(1.5)
             ending = input("GAME OVER, RESTART?:")
             if ending == "Yes":
@@ -259,7 +259,7 @@ if ending == "^^vv<><>BA-" :
                     time.sleep(1.5)
                     print("This causes your ears to bleed and you to go to the hospital.")
                     time.sleep(1.5)
-                    print("[Insert Dark Souls: 'YOU DIED' here]")
+                    print("[Insert Dark Souls: YOU DIED here]")
                 time.sleep(1.5)
                 ending = input("GAME OVER, RESTART?:")
                 if ending == "Yes":
@@ -298,6 +298,7 @@ if ending == "^^vv<><>BA-" :
                         if location == "NG+4":
                             time.sleep(1.5)
                             print("'Alone! Infection! Void! S**t attacking from the other side, yuck! Alone! Infection! Chaos! A-B-C-D-E-F, go! Checkmate (checkmate), informed consent (oh, yeah). It increases words. Relation (relation). Duration (oh, yeah). Build and realize. MESSIAH! Will not come. Parallax! Paradox! Paradigm will attack! We've got only one sky! Blue, red, and black paranoia. What is it like to you? No one can see the colors but you! Alone! Infection! Void! S**t attacking from the other side yuck! Alone! Infection! Chaos! A-B-C-D-E-F, go! Light and darkness invisible (oh, yeah). Your soul dissipates. Destruction  re-reduction (oh, yeah). TOWER OF BABEL! MESSIAH! Will not come. Parallax! Paradox! Paradigm will attack! We've got only one sky! Blue, red, and black paranoia. What is it like to you? No one can see the colors but you! ...I had to run away from here...Before the storm hit...We see eye to eye...We see eye to eye...I told me, without you! We've got only one sky! Blue, red, and black (BLUE, RED, AND BLACK) paranoia (paranoia)! What is it like to you? (LIKE TO YOU!) No one can see the colors but you! We've got only one sky! Joy, grief, and fun PARANOIA! What is it like to you? No one can see the colors, the colors, but you!'")
+
                         if location == "Merry-Go-Round":
                             time.sleep(1.5)
                             print("Good choice!")
@@ -320,7 +321,7 @@ if ending == "^^vv<><>BA-" :
                         time.sleep(1.5)
                         print("This causes your ears to bleed and you to go to the hospital.")
                         time.sleep(1.5)
-                        print("[Insert Dark Souls: 'YOU DIED' here]")
+                        print("[Insert Dark Souls: YOU DIED here]")
                     time.sleep(1.5)
                     ending = input("GAME OVER, RESTART?:")
                     if ending == "Yes":
