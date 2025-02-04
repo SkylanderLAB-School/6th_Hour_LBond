cc178d46a13ce5eddd7ecb36e0156565a79b92bd
Logan Bond <lb125093@wpsok.org>
2025-02-04 13:29:45 -0600

    Scenario 7 has been de-logan'd

--- Scnario 7.py
+++ Scnario 7.py
@@ -8,7 +8,7 @@ listAverage = 0
 
 def final_average():
     global listAverage
-    listAverage = (Stat_Block[0] + Stat_Block[1] + Stat_Block[2] + Stat_Block[3] + Stat_Block[4] + Stat_Block[5])/6#Calculate the sum of the list by the length of the list here
+    listAverage = sum(Stat_Block)/len(Stat_Block)#Calculate the sum of the list by the length of the list here
     return listAverage
 
 final_average()
