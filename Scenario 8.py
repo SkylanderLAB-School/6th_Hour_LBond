c331d1c625471528d95a8553281a0491b92ae22b
Logan Bond <lb125093@wpsok.org>
2025-03-10 14:07:32 -0500

    Started SC8

---
+++ Scenario 8.py
@@ -0,0 +1,53 @@
+#Name: Logan Bond
+#Class: 6th Hour
+#Assignment: Scenario8
+import random
+
+
+
+#Scenario 8:
+#With a fresh perspective, the team lead wants you to look back and refactor the old combat code to
+#be streamlined with classes so the character and enemy stats won't be built in bulky dictionaries anymore.
+
+#(Translation: Rebuild Scenario 3 using classes instead of dictionaries, include the combat test code
+#below as well.)
+
+class Character:
+    def __init__(self, name, species, type, health, ac, atk_mod, damage):
+        self.name = name
+        self.species = name
+        self.type = name
+        self.health = name
+        self.ac = name
+        self.atk_mod = name
+        self.damage = name
+
+
+
+#Characters
+#LaeZel - Non-school appropriate word
+#Shadowheart - Who?
+#Gale - Gale
+#Astarion - Interview with a Vampire
+#Mr. Magic Man
+
+
+
+#Enemies
+#Creeping Chrysanthemum
+#Gatling Groink
+#Careening Dirigibug
+#Man-At-Legs
+#Titan Dweevil
+
+
+Alien = Character()
+Dead = Character()
+Magic = Character()
+Vampire = Character()
+Tarnished = Character()
+Flower = Character()
+GunFish = Character()
+Blimp = Character()
+GunSpider = Character()
+Louis = Character()
